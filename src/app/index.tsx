b9195035666abd9682b4caac1fa4d530f452b9bb
tzptzptzptzptzp <git@tzp.pink>
2025-07-15 00:51:41 +0900

    ✨ useConfigフックを追加し、テーマカラーをスタイルに適用

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,28 @@
+"use client";
 import { Editor } from "@/components/features/editor";
 import { Preview } from "@/components/features/preview";
+import { useConfig } from "@/hooks/useConfig";
 
 export const HomeIndex = () => {
+  const { themeColor } = useConfig();
+
   return (
-    <div className="flex items-center justify-around h-dvh">
-      <div>
-        <Preview />
-      </div>
-      <div>
-        <Editor />
-      </div>
-    </div>
+    <>
+      <style>{`
+        :root {
+            --theme-main-color: ${themeColor.mainColor};
+            --theme-background-color: ${themeColor.backgroundColor};
+            --theme-font-color: ${themeColor.fontColor};
+        }
+      `}</style>
+      <main className="flex items-center justify-around h-dvh">
+        <div>
+          <Preview />
+        </div>
+        <div>
+          <Editor />
+        </div>
+      </main>
+    </>
   );
 };
