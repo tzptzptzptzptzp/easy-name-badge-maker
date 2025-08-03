e20a0e1ce390df58e164985e7fdce06e04a3195f
tzptzptzptzptzp <git@tzp.pink>
2025-08-04 00:57:04 +0900

    ♻️ Previewコンポーネントのインポート順を整理し、不要なコードを削除

--- src/components/features/preview/index.tsx
+++ src/components/features/preview/index.tsx
@@ -2,15 +2,14 @@
 import { useConfig } from "@/hooks/useConfig";
 import { PreviewHeader } from "./Header";
 import { PreviewIcon } from "./Icon";
+import { PreviewBadge } from "./Badge";
+import { PreviewPR } from "./PR";
 import { PreviewName } from "./Name";
-import Image from "next/image";
 import { PreviewActivity } from "./Activity";
 import { PreviewComment } from "./Comment";
 import { PreviewMakerLogo } from "./MakerLogo";
-import { PreviewQRCode } from "./QRCode";
 import { PreviewGakucho } from "./Gakucho";
-import { PreviewBadge } from "./Badge";
-import { PreviewPR } from "./PR";
+import { PreviewQRCode } from "./QRCode";
 
 export const Preview = () => {
   const { scaledFrameSize } = useConfig();
@@ -31,15 +30,6 @@ export const Preview = () => {
         <PreviewMakerLogo />
         <PreviewGakucho />
         <PreviewQRCode />
-      </div>
-      <div className="absolute inset-0 select-none pointer-events-none opacity-30">
-        <Image
-          src={`/images/sample.png`}
-          alt="リベシティ"
-          width={1200}
-          height={1200}
-          className="w-full h-full object-cover"
-        />
       </div>
     </div>
   );
