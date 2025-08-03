896884f49ece31e942ea5d85f36ca5fa0a260fa1
tzptzptzptzptzp <git@tzp.pink>
2025-08-04 01:13:55 +0900

    ⚡️ 状態をローカルストレージに保存するように変更

--- src/hooks/useUserStore.ts
+++ src/hooks/useUserStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type User = {
   name: string;
@@ -8,7 +9,14 @@ type UserStore = User & {
   setName: (name: string) => void;
 };
 
-export const useUserStore = create<UserStore>((set) => ({
-  name: "",
-  setName: (name: string) => set({ name }),
-}));
+export const useUserStore = create<UserStore>()(
+  persist(
+    (set) => ({
+      name: "",
+      setName: (name: string) => set({ name }),
+    }),
+    {
+      name: "user-store",
+    }
+  )
+);
