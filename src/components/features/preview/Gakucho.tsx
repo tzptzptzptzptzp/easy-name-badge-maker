a504a64245ce6d59c52e33f5cb1c77c90aa43d9f
tzptzptzptzptzp <git@tzp.pink>
2025-08-07 19:47:01 +0900

    🩹 プレビューの位置を調整

--- src/components/features/preview/Gakucho.tsx
+++ src/components/features/preview/Gakucho.tsx
@@ -9,7 +9,12 @@ export const PreviewGakucho = () => {
   return (
     <div
       className="absolute"
-      style={{ bottom: scale(12.5), left: scale(206), width: scale(88) }}
+      style={{
+        bottom: scale(62),
+        left: scale(250),
+        transform: "translate(-50%,50%)",
+        width: scale(88),
+      }}
     >
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
